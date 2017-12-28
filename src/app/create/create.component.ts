426dec69528716a77ddca1738b1118b405c80d45
cappuccinocat <xinglydia@gmail.com>
2017-12-27 19:14:34 -0500

    waypoint id is now row id

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -19,14 +19,7 @@ import { TableDataSource } from 'angular4-material-table';
 })
 
 export class CreateComponent implements OnInit {
-  //dataChange: BehaviorSubject<Waypoint[]> = new BehaviorSubject<Waypoint[]>([]);
-  //get data(): Waypoint[] { return this.dataChange.value; }
-  //exampleDatabase = new ExampleDatabase();
-  //dataSource: ExampleDataSource | null;
   waypointService: WaypointService;
-  //dataStream: Observable<Waypoint>;
-
-
   waypoints: Waypoint[] = [];
 
   constructor(waypointService: WaypointService) {
@@ -36,8 +29,7 @@ export class CreateComponent implements OnInit {
   displayedColumns = ['id', 'x', 'y', 'theta', 'actionsColumn'];
 
   @Input() waypointList = [
-    { id: 15, x: 5, y: 6, theta: 2 },
-    { id: 50, x: 3, y: 3, theta: 1 },
+    { id: 0, x: 5, y: 6, theta: 2 }
   ];
 
   @Output() waypointListChange = new EventEmitter<Waypoint[]>();
@@ -45,67 +37,17 @@ export class CreateComponent implements OnInit {
   dataSource: TableDataSource<Waypoint>;
 
   ngOnInit() {
-    //console.log(this.waypoints);
     this.dataSource = new TableDataSource<any>(this.waypointList, Waypoint);
     this.dataSource.datasourceSubject.subscribe(waypointList => this.waypointListChange.emit(waypointList));
+
     this.waypointListChange.asObservable().subscribe(data => {
       data.forEach(item => {
-        this.waypointService.updateWaypoint(item as Waypoint)
+        this.waypointService.updateWaypoint({id: data.length, x: item.x, y: item.y, theta: item.theta} as Waypoint)
         .subscribe(waypoint => {
           this.waypoints.push(waypoint);
         })
       })
-      /**this.waypointService.updateWaypoints(data as Waypoint[])
-      .subscribe(waypoints => {
-        this.waypoints.push(waypoints);
-      });**/
       this.waypointService.getWaypoints().subscribe(data => console.log(data));
     });
-      /**{ data
-      .forEach(item => {
-        this.waypointService.addWaypoint(item as Waypoint)
-        .subscribe(waypoint => {
-          this.waypoints.push(waypoint);
-        })
-      });
-    });**/
   }
-
-  getWaypoints(): void {
-    this.waypointService.getWaypoints()
-      .subscribe(waypoints => this.waypoints = waypoints.slice(1, 5));
-  }
-
-  add(params: any): void {
-    //x: number, y: number, theta: number
-    //name = name.trim();
-    //if (!name) { return; }
-    this.dataSource.createNew();
-    //console.log(this.waypointList);
-
-
-
-    //console.log(this.waypointList);
-
-    //this.update(5);
-
-  }
-
-  delete(waypoint: Waypoint): void {
-    this.waypoints = this.waypoints.filter(h => h !== waypoint);
-    this.waypointService.deleteWaypoint(waypoint).subscribe();
-  }
-
-  edit(params: any) {
-    params.confirmEditCreate()
-    //console.log(params);
-  }
-
-  update(params: any){
-    this.waypointService.addWaypoint({ id: 3, x: 5, y: 4, theta: 2} as Waypoint)
-      .subscribe(waypoint => {
-        this.waypoints.push(waypoint);
-      });
-  }
-
 }
--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -17,9 +17,7 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const waypoints = [
-      { id: 11, x: 5, y: 6, theta: 90 },
-      { id: 11, x: 5, y: 7, theta: 90 },
-      { id: 11, x: 5, y: 8, theta: 90 }
+      { id: 0, x: 5, y: 6, theta: 90 }
     ];
     return {waypoints};
   }
